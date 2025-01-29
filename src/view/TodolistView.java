25004dcd622ddcfb7e6846604ed97059b7f1ee95
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-01-29 12:29:16 +0700

    add view show todolist

--- src/view/TodolistView.java
+++ src/view/TodolistView.java
@@ -1,8 +1,36 @@
 package view;
 
-public class TodolistView {
-    public void showTodolist(){
+import service.TodolistService;
+import util.InputUtil;
 
+public class TodolistView {
+    private TodolistService todolistService;
+
+    public TodolistView(TodolistService todolistService) {
+        this.todolistService = todolistService;
+    }
+
+    public void showTodolist(){
+        while (true){
+            todolistService.showTodolist();
+
+            System.out.println("Menu : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+
+            var input = InputUtil.input("Pilih");
+
+            if(input.equals("1")){
+                addTodolist();
+            } else if (input.equals("2")) {
+                removeTodolist();
+            } else if (input.equals("x")) {
+                break;
+            } else {
+                System.out.println("Choice invalid");
+            }
+        }
     }
 
     public void addTodolist(){
